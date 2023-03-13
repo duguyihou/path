688b9099136382d66c91cd0e6ebd5a1c1e0c4527
Kong <duguyihou@gmail.com>
2023-03-13 21:18:03 +1100

    feat: theme (#26)

--- src/features/tripPlanner/components/nearbyCard/NearbyCard.tsx
+++ src/features/tripPlanner/components/nearbyCard/NearbyCard.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {theme} from 'shared/theme';
 
 import {NearbyCardProps} from './NearbyCard.types';
 
 const NearbyCard = (props: NearbyCardProps) => {
-  const {disassembledName, id} = props;
+  const {disassembledName, id, distance} = props;
   const handlePress = () => console.log('🐵  ------ ', id);
+  console.log('🐵 props ------ ', props);
   return (
     <TouchableOpacity onPress={handlePress} style={styles.container}>
-      <Text>{disassembledName}</Text>
+      <Text style={styles.stopName}>{disassembledName}</Text>
+      <Text style={styles.distance}>{distance}m</Text>
     </TouchableOpacity>
   );
 };
@@ -18,5 +21,14 @@ export default NearbyCard;
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+    backgroundColor: theme.secondary,
+  },
+  stopName: {
+    fontSize: 18,
+    paddingBottom: 10,
+  },
+  distance: {
+    fontSize: 12,
+    color: theme.grey_2,
   },
 });
